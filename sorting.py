92dc7b6f0063dbc4abf99865bafb58cbb3cc0b00
vojtech-p <vojtech.pavlik@centrum.cz>
2024-04-15 08:49:21 +0200

    'cs'

--- sorting.py
+++ sorting.py
@@ -1,4 +1,5 @@
 import os
+import csv
 
 
 def read_data(file_name):
@@ -10,10 +11,37 @@ def read_data(file_name):
     """
     cwd_path = os.getcwd()
     file_path = os.path.join(cwd_path, file_name)
+    data = {}
+    with open(file_path, mode='r', newline='') as csvfile:
+        csv_reader = csv.reader(csvfile)
+        # Přičtení hlavičky
+        headers = next(csv_reader)
+
+        # Inicializacee - Pro každý sloupec - prázdný seznam v datovém slovníku
+        for header in headers:
+            data[header] = []
+
+        # Načtení dat ze zbývajících řádků csv souboru
+        for row in csv_reader:
+            # Přidání čísel z řádku do odpovídajícího seznamu
+            for i, value in enumerate(row):
+                data[headers[i]].append(float(value))
+        return data
+
+
+def selection_sort(data):
+    print(data)
+#    for nazev, seznam in data:
+
+    return
 
 
 def main():
-    pass
+    # Nazev csv souboru
+    filename = 'numbers.csv'
+    data = read_data(filename)
+#    print(data)
+    print(selection_sort(data))
 
 
 if __name__ == '__main__':
